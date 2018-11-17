753af750c7c1c6dc9071c52f83a8398cebb5332b
wellingtonapolonio <wellingtonpoloniosilva@gmail.com>
2018-11-17 18:54:48 -0200

    first commit

---
+++ src/application/Program.java
@@ -0,0 +1,18 @@
+package application;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		String a = "Maria";
+		String b = "Alex";
+		System.out.println(a.equals(b));
+		
+		String a1 = "Maria";
+		String b2 = "Alex";
+		System.out.println(a1.hashCode());
+		System.out.println(b2.hashCode());
+
+	}
+
+}
